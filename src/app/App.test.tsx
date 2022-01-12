867067b6b00de60da07a051db8e6ddb707ed9ced
jaywcjlove <398188662@qq.com>
2022-01-12 16:26:15 +0800

    test: fix test case error.

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import App from './App';
 
 it('renders without crashing', () => {
@@ -10,11 +10,12 @@ it('renders without crashing', () => {
 });
 
 it('App Click!`', async () => {
-  const { getByTitle } = render(<App />);
+  render(<App />);
   fireEvent.keyDown(document, { key: 'J', code: 'KeyJ', ctrlKey: true, shiftKey: true });
-  const input = getByTitle('event.keyCode');
+  const input = screen.getByTitle<HTMLElement>('event.keyCode');
   expect(input.className).toEqual('deprecated');
   fireEvent.click(input);
   fireEvent.click(input);
-  expect(input.parentElement!.className).toEqual('w-copy-to-clipboard copied');
+  // eslint-disable-next-line testing-library/no-node-access
+  expect(input.parentElement?.className).toEqual('w-copy-to-clipboard copied');
 });
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -24,7 +24,7 @@ const App = () => {
       'metaKey',
       'repeat',
       'shiftKey',
-      'char',
+      // 'char',
       'charCode',
       'keyCode',
       // 'keyIdentifier',
