e0fb1b8ac0081d8d6b2adb5eccb0a1133c2963de
jaywcjlove <398188662@qq.com>
2023-08-12 10:30:25 +0800

    chore: update webstie style.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,10 +5,95 @@ import { lightTheme } from '@uiw/react-json-view/light';
 import CopyToClipboard from '@uiw/react-copy-to-clipboard';
 import GitHubCorners from '@uiw/react-github-corners';
 import '@wcj/dark-mode';
-import './App.css';
+import styled, { css } from 'styled-components';
+
+const Wrapper = styled.main`
+  text-align: center;
+`;
+
+const CopiedInfo = styled.div`
+  position: fixed;
+  top: 10px;
+  left: 10px;
+  background-color: #03a132;
+  color: #fff;
+  padding: 2px 5px;
+  border-radius: 2px;
+`;
+
+const Header = styled.header`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Keyboard = styled.div`
+  display: flex;
+  & > span + span {
+    margin-left: 10px;
+    display: inline-block;
+  }
+  & .deprecated {
+    background-color: #ff9696;
+    text-decoration: line-through;
+  }
+  & .copied kbd {
+    background: #03a132;
+  }
+  & kbd {
+    font-size: 18px;
+    min-width: 45px;
+    min-height: 23px;
+    font-weight: bold;
+    display: inline-block;
+    background: #eff0f2;
+    background: var(--color-theme-text);
+    color: var(--color-theme-bg);
+    border-radius: 3px;
+    padding: 5px 10px;
+    border-top: 1px solid #f5f5f5;
+    box-shadow: inset 0 0 25px #e8e8e8, 0 1px 0 #c3c3c3, 0 2px 0 #c9c9c9, 0 2px 3px #333;
+    text-shadow: 0px 1px 0px #f5f5f5;
+    cursor: pointer;
+    user-select: none;
+    transition: background-color 0.3s;
+  }
+`;
+
+const KbdKey = styled.kbd`
+  font-size: 40vmin;
+  text-align: center;
+`;
+
+const view = css`
+  display: inline-block;
+  color: var(--color-theme-text);
+  text-decoration: none;
+  text-align: center;
+  margin: 20px auto;
+  padding: 15px 20px;
+  background-color: var(--color-json-bg);
+  border-radius: 5px;
+  min-width: 24px;
+`;
+
+const Help = styled.div`
+  ${view}
+  font-size: 26px;
+  box-shadow: 0 14px 26px rgb(0 0 0 / 4%);
+`;
+
+const JsonView = styled.div`
+  ${view}
+  text-align: initial;
+  min-width: 320px;
+  font-size: 16px;
+  background-color: var(--color-json-bg);
+`;
 
 type Writeable<T> = { -readonly [P in keyof T]: T[P] };
-
 type KeyboardEventType = Writeable<KeyboardEvent>;
 
 const App = () => {
@@ -77,16 +162,16 @@ const App = () => {
   }, []);
   const style = (theme === 'dark' ? darkTheme : lightTheme) as React.CSSProperties;
   return (
-    <div className="App">
-      <dark-mode permanent light="Light" dark="Dart" style={{ position: 'fixed', top: '6px', left: '10px' }}></dark-mode>
+    <Wrapper>
+      <dark-mode permanent light="Light" dark="Dart" style={{ position: 'fixed', top: '6px', left: '10px', fontSize: 18 }} />
       <GitHubCorners fixed size={56} target="_blank" href="https://github.com/uiwjs/keycode-info" />
-      {copied && <div className="copied-info">copied</div>}
-      <header className="App-header">
-        {!data && <div className="help">Press any key to get the JavaScript event keycode</div>}
+      {copied && <CopiedInfo>copied</CopiedInfo>}
+      <Header>
+        {!data && <Help>Press any key to get the JavaScript event keycode</Help>}
         {data && (
           <Fragment>
-            <kbd className="key-which">{data.which}</kbd>
-            <div className="keyboard">
+            <KbdKey className="key-which">{data.which}</KbdKey>
+            <Keyboard>
               <CopyToClipboard onClick={handleClick} text={data.key}>
                 <kbd title="event.key">{data.key}</kbd>
               </CopyToClipboard>
@@ -106,14 +191,14 @@ const App = () => {
               <CopyToClipboard onClick={handleClick} text={`${data.code}`}>
                 <kbd title="event.code">{data.code}</kbd>
               </CopyToClipboard>
-            </div>
-            <div className="json-view">
+            </Keyboard>
+            <JsonView>
               <JsonViewer value={data} style={{ ...style, '--w-rjv-background-color': 'transparent' } as React.CSSProperties} />
-            </div>
+            </JsonView>
           </Fragment>
         )}
-      </header>
-    </div>
+      </Header>
+    </Wrapper>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,34 @@
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import { createGlobalStyle } from 'styled-components';
 import App from './app/App';
+import { Fragment } from 'react';
+
+export const GlobalStyle = createGlobalStyle`
+  [data-color-mode*='dark'], [data-color-mode*='dark'] body {
+    --color-theme-bg: #282c34 !important;
+    --color-json-bg: #2c303b;
+  }
+  [data-color-mode*='light'], [data-color-mode*='light'] body {
+    --color-json-bg: #f4f4f4 !important;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-size: 14px;
+    background-color: #282c34;
+  }
+`;
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
-root.render(<App />);
+root.render(
+  <Fragment>
+    <GlobalStyle />
+    <App />
+  </Fragment>,
+);
